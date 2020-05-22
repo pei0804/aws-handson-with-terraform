2873b40fa749cd943d931c4e889565af0a37da0e
pei <peeeei0804@gmail.com>
2020-05-22 11:15:08 +0900

    Update main.tf

--- part1/main.tf
+++ part1/main.tf
@@ -5,17 +5,15 @@ locals {
 #####
 # VPC
 #####
-resource "aws_subnet" "public" {
-  vpc_id     = aws_vpc.handson.id
-  cidr_block = "10.0.1.0/24"
-  availability_zone = "ap-northeast-1a"
-  map_public_ip_on_launch = true
+resource aws_vpc handson {
+  cidr_block = "10.0.0.0/16"
+  enable_dns_support = true
+  enable_dns_hostnames = true
 
   tags = {
     Name = local.name
   }
 }
-
 
 ########
 # Subnet
