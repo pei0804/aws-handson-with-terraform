8edb5302b7f7bf0bcc8bd0c6e066033944ee7011
pei <peeeei0804@gmail.com>
2020-05-22 12:32:44 +0900

    Update main.tf

--- part1/main.tf
+++ part1/main.tf
@@ -2,41 +2,40 @@ locals {
   name = "handson"
 }
 
-#####
+######
 # VPC
-#####
+######
 resource aws_vpc handson {
-  cidr_block = "10.0.0.0/16"
+  cidr_block       = "10.0.0.0/16"
 
   tags = {
     Name = local.name
   }
 }
 
-########
+#########
 # Subnet
-########
+#########
 resource aws_subnet handson {
   vpc_id     = aws_vpc.handson.id
   cidr_block = "10.0.1.0/24"
+  availability_zone = "ap-northeast-1a"
 
   tags = {
     Name = local.name
   }
 }
 
-##############
-# EC2 Instance
-##############
-data aws_ssm_parameter amzn2_ami {
+data aws_ssm_parameter amzn_ami {
   name = "/aws/service/ami-amazon-linux-latest/amzn2-ami-hvm-x86_64-gp2"
 }
 
+######
+# EC2
+######
 resource aws_instance handson {
-  ami = data.aws_ssm_parameter.amzn2_ami.value
+  ami           = data.aws_ssm_parameter.amzn_ami.value
   instance_type = "t2.micro"
-
-  subnet_id = aws_subnet.handson.id
 
   tags = {
     Name = local.name
