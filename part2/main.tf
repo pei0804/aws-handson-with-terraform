78f4e8d3f13f412cd4ceb123fde64685691f2881
pei <peeeei0804@gmail.com>
2020-05-23 17:18:30 +0900

    Update main.tf

--- part2/main.tf
+++ part2/main.tf
@@ -19,6 +19,7 @@ resource aws_vpc handson {
 resource aws_subnet public {
   vpc_id     = aws_vpc.handson.id
   cidr_block = "10.0.1.0/24"
+  availability_zone = "ap-northeast-1a"
   map_public_ip_on_launch = true
 
   tags = {
